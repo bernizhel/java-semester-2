554f085732702d3cc4a9124dfd7fdf3af52bdcc4
bernizhel <detrop.1995@gmail.com>
2023-03-30 20:13:49 +0300

    chore: move task1 to the ru.vsuet package as Main

---
+++ src/main/java/ru/vsuet/Main.java
@@ -0,0 +1,15 @@
+package ru.vsuet;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        if (args.length == 0) {
+            System.out.println("There is no args");
+        }
+
+        for (int i = 0; i < args.length; i++) {
+            System.out.printf("%d. arg %d - %s%n", i + 1, i + 1, args[i]);
+        }
+    }
+}

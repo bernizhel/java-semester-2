0caa3e96601ae9f4f2a1d1af7f78e90fdeaef3e5
bernizhel <detrop.1995@gmail.com>
2023-03-30 23:52:50 +0300

    feat: add 16 math problems solved

---
+++ src/main/java/ru/vsuet/math/Seven.java
@@ -0,0 +1,17 @@
+package ru.vsuet.math;
+
+import java.util.Scanner;
+
+public class Seven {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int n = scanner.nextInt();
+
+        System.out.println("Closest one is: " + getClosestSquaredNumber(n));
+    }
+
+    public static long getClosestSquaredNumber(int n) {
+        return Math.round(Math.sqrt(n));
+    }
+}

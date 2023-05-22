aa6e8ff50744ec3e98a75c79a391ba5c5344d739
bernizhel <detrop.1995@gmail.com>
2023-05-22 12:52:40 +0300

    fix(generic_matrix): self implemented number to string converter usage in tests

--- src/test/java/ru/vsuet/my_matrix/generic/MatrixProvider.java
+++ src/test/java/ru/vsuet/my_matrix/generic/MatrixProvider.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.ArgumentsProvider;
 
-//import java.math.BigDecimal;
 import java.math.BigDecimal;
 import java.util.stream.Stream;
 
--- src/test/java/ru/vsuet/my_matrix/generic/MatrixTest.java
+++ src/test/java/ru/vsuet/my_matrix/generic/MatrixTest.java
@@ -12,8 +12,26 @@ public class MatrixTest {
     @ArgumentsSource(MatrixProvider.class)
     public <N extends Number> void fillWithOnes_IntegerMatrix_beOk(Matrix<N> matrix, N fillingConstant, N sumResult, N productResult, N negativeSumResult) {
         matrix.fill(fillingConstant);
-        assertEquals(sumResult, matrix.getSumAboveMainDiagonal());
-        assertEquals(productResult, matrix.getProductOfMainDiagonal());
-        assertEquals(negativeSumResult, matrix.getNegativeSumBelowMainDiagonal());
+        assertEquals(StringConverter.convert(sumResult), StringConverter.convert(matrix.getSumAboveMainDiagonal()));
+        assertEquals(StringConverter.convert(productResult), StringConverter.convert(matrix.getProductOfMainDiagonal()));
+        assertEquals(StringConverter.convert(negativeSumResult), StringConverter.convert(matrix.getNegativeSumBelowMainDiagonal()));
+    }
+
+    private static class StringConverter {
+        public static String convert(Number number) {
+            String stringified = number.toString();
+
+            int startIndex = -1;
+            do {
+                startIndex++;
+            } while (stringified.charAt(startIndex) == '0');
+
+            int endIndex = stringified.length();
+            do {
+                endIndex--;
+            } while (stringified.charAt(endIndex) == '0');
+
+            return stringified.substring(startIndex, endIndex);
+        }
     }
 }

dd2d3b1787b1a009397a7f5b6e1efe72f1f247b3
bernizhel <detrop.1995@gmail.com>
2023-05-25 17:20:31 +0300

    docs(homework_hashtable): documentation for the `HashingAlgorithm`

--- src/main/java/ru/vsuet/homework_hashtable/hashing_algorithm/HashingAlgorithm.java
+++ src/main/java/ru/vsuet/homework_hashtable/hashing_algorithm/HashingAlgorithm.java
@@ -7,13 +7,24 @@ package ru.vsuet.homework_hashtable.hashing_algorithm;
  * @see ru.vsuet.homework_hashtable.IHashtable
  */
 public class HashingAlgorithm {
+    /**
+     * Calculates object's hash by a custom algorithm.
+     * Returns a random integer representing a hash.
+     * It can return any number from {@code Integer.MIN_VALUE} to {@code Integer.MAX_VALUE}.
+     *
+     * @param object object you want to hash
+     * @param <K>    object's type
+     * @return object's hash represented as an integer.
+     * @see Integer#MIN_VALUE
+     * @see Integer#MAX_VALUE
+     */
     public static <K> int hashCode(K object) {
         if (object instanceof Number) {
             return ((Number) object).intValue();
         }
 
         if (object instanceof String) {
-            return ((String) object).chars().reduce(0, (accumulator, nextCharacter) -> accumulator + (nextCharacter << 5 - nextCharacter));
+            return ((String) object).chars().reduce(0, (acc, nextChar) -> acc + (nextChar << 5 - nextChar));
         }
 
         return object.hashCode();
